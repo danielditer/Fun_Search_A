f5eb966bfc28abf9858e25a1e6809e72efe59a27
manu863018 <manuel.valdez@fundacion-jala.org>
2018-03-25 21:21:29 -0400

    First Controller class Validator.

--- src/main/java/com/fundation/search/controller/Validator.java
+++ src/main/java/com/fundation/search/controller/Validator.java
@@ -1,4 +1,48 @@
+/**
+ * @(#)Validator.java  03/25/18
+ *
+ * Copyright (c) 2018 Jala Foundation.
+ * Cochabamba, Bolivia.
+ *
+ * Project Search for Prog102.
+ */
 package com.fundation.search.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * @version
+ * 25 Mar 2018  * @Juan Manuel
+ */
 public class Validator {
+
+    static final List<Character> INVALID_CHARACTERS = new ArrayList<>();
+    /**
+     * static method to fill list.
+     */
+    static {
+        INVALID_CHARACTERS.add('/');
+        INVALID_CHARACTERS.add(':');
+        INVALID_CHARACTERS.add('*');
+        INVALID_CHARACTERS.add('?');
+        INVALID_CHARACTERS.add('<');
+        INVALID_CHARACTERS.add('>');
+        INVALID_CHARACTERS.add('|');
+        INVALID_CHARACTERS.add('"');
+    }
+
+    /**
+     * method to validate string.
+     * @param name file name.
+     * @return boolean value is valid.
+     */
+    public boolean isAValidName(String name) {
+        char[] nameCharacters = name.toCharArray();
+        for (char value : nameCharacters) {
+            if (INVALID_CHARACTERS.contains(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/com/fundation/search/controller/ValidatorTest.java
+++ src/test/java/com/fundation/search/controller/ValidatorTest.java
@@ -1,5 +1,103 @@
-import static org.junit.Assert.*;
+/**
+ * @(#)ValidatorTest.java  03/25/18
+ *
+ * Copyright (c) 2018 Jala Foundation.
+ * Cochabamba, Bolivia.
+ *
+ * Project Search for Prog102.
+ */
+package com.fundation.search.controller;
 
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @version
+ * 25 Mar 2018  * @Juan Manuel
+ */
 public class ValidatorTest {
+    private Validator validator;
 
+    /**
+     * method to initialize object.
+     */
+    @Before
+    public void init() {
+        validator = new Validator();
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameSlash() {
+        assertFalse("A name with / is not valid", validator.isAValidName("/"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameTwoPoints() {
+        assertFalse("A name with : is not valid", validator.isAValidName(":"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameAsterisk() {
+        assertFalse("A name with * is not valid", validator.isAValidName("*"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameQuestionMark() {
+        assertFalse("A name with ? is not valid", validator.isAValidName("?"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameLessThan() {
+        assertFalse("A name with < is not valid", validator.isAValidName("<"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameMoreThan() {
+        assertFalse("A name with > is not valid", validator.isAValidName(">"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNamePipe() {
+        assertFalse("A name with | is not valid", validator.isAValidName("|"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameWithTwoPoints() {
+        assertFalse("file:name is not valid", validator.isAValidName("file:name"));
+    }
+
+    /**
+     * First test.
+     */
+    @Test
+    public void testIsValidNameValidFileName() {
+        assertTrue("file_name is a valid name", validator.isAValidName("file_name"));
+    }
 }
