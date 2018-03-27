26202f86e5ba41c89087927e0715b2cdf5af56c9
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-03-27 16:18:39 -0400

    Added MainView

---
+++ src/main/java/com/fundation/search/view/MainView.java
@@ -0,0 +1,95 @@
+package com.fundation.search.view;
+
+import java.awt.Container;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+
+import info.clearthought.layout.TableLayout;
+import net.miginfocom.swing.MigLayout;
+
+/**
+ * @author Daniel Caballero
+ */
+public class MainView extends JFrame {
+    /**
+     * MainView constructor.
+     */
+    public MainView() {
+        initComponents();
+    }
+
+    /**
+     * Initializes variables.
+     */
+    protected void initComponents() {
+        menuBar = new JMenuBar();
+        menuFile = new JMenu();
+        menuEdit = new JMenu();
+        menuView = new JMenu();
+        menuHelp = new JMenu();
+        tabbedPane = new JTabbedPane();
+        panelNormalSearch = new PanelNormalSearch();
+        panelMultimediaSearch = new JPanel();
+        panelTerminalSearch = new JPanel();
+        panelTabbedPane = new JPanel();
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+                "hidemode 3",
+                // columns
+                "[fill]" + "[fill]",
+                // rows
+                "[]" + "[]" + "[]"));
+        //======== menuBar ========
+        //======== menuFile ========
+        menuFile.setText("File");
+        menuBar.add(menuFile);
+        //======== menuEdit ========
+        menuEdit.setText("Edit");
+        menuBar.add(menuEdit);
+        //======== menuView ========
+        menuView.setText("View");
+        menuBar.add(menuView);
+        //======== menuHelp ========
+        menuHelp.setText("Help");
+        menuBar.add(menuHelp);
+        setJMenuBar(menuBar);
+        //======== panelNormalSearch ========
+        // JFormDesigner evaluation mark
+
+        tabbedPane.addTab("Normal Search", panelNormalSearch);
+
+        //======== panelMultimediaSearch ========
+        panelMultimediaSearch.setLayout(new TableLayout(new double[][]{
+                {TableLayout.PREFERRED, TableLayout.PREFERRED},
+                {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}}));
+        tabbedPane.addTab("Multimedia Search", panelMultimediaSearch);
+        //======== panelTerminalSearch ========
+        panelTerminalSearch.setLayout(new TableLayout(new double[][]{
+                {TableLayout.PREFERRED, TableLayout.PREFERRED},
+                {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}}));
+        tabbedPane.addTab("Terminal Search", panelTerminalSearch);
+        contentPane.add(tabbedPane, BorderLayout.CENTER);
+        //======== panelTabbedPane ========
+        panelTabbedPane.setLayout(new BorderLayout());
+        contentPane.add(panelTabbedPane, BorderLayout.CENTER);
+        setVisible(true);
+        pack();
+        setLocationRelativeTo(getOwner());
+    }
+
+    private JMenuBar menuBar;
+    private JMenu menuFile;
+    private JMenu menuEdit;
+    private JMenu menuView;
+    private JMenu menuHelp;
+    private JTabbedPane tabbedPane;
+    private JPanel panelMultimediaSearch;
+    private JPanel panelTerminalSearch;
+    private JPanel panelTabbedPane;
+    private PanelNormalSearch panelNormalSearch;
+}
