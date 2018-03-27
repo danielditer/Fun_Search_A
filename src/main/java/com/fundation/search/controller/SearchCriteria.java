8f07ba26c120ac411b2a2525743755a12b9a5278
manu863018 <manuel.valdez@fundacion-jala.org>
2018-03-27 18:18:36 +0000

    Search criteria and Controller class.

---
+++ src/main/java/com/fundation/search/controller/SearchCriteria.java
@@ -0,0 +1,22 @@
+package com.fundation.search.controller;
+
+public class SearchCriteria {
+    private String name;
+    private String path;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+}
