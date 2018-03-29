e0031a7fa209cb0b2e2604f5d1c826046e4d4bde
manu863018 <manuel.valdez@fundacion-jala.org>
2018-03-29 18:18:41 +0000

    Add comments to main.

--- src/main/java/com/fundation/search/Main.java
+++ src/main/java/com/fundation/search/Main.java
@@ -1,3 +1,9 @@
+/**
+ * @(#)Main.java  03/29/18.
+ * Copyright (c) 2018 Jala Foundation.
+ * Cochabamba, Bolivia.
+ * Project Search for Prog102.
+ */
 package com.fundation.search;
 
 import com.fundation.search.controller.SearchController;
@@ -5,9 +11,15 @@ import com.fundation.search.model.SearchFiles;
 import com.fundation.search.view.MainView;
 
 /**
- * Main class.
+ * Main class to run project.
+ * @version
+ * 29 Mar 2018  * @Juan Manuel
  */
 public class Main {
+    /**
+     * Method main to run project.
+     * @param args
+     */
     public static void main (String[] args) {
         SearchFiles searchFile = new SearchFiles();
         MainView mainView = new MainView();
