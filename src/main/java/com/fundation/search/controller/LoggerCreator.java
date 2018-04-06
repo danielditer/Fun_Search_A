d3423e2a96f481034b554315f2484bccdc311001
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-04-06 13:52:22 -0400

    Logger documentation added, and implements some methods to create a log file

--- src/main/java/com/fundation/search/controller/LoggerCreator.java
+++ src/main/java/com/fundation/search/controller/LoggerCreator.java
@@ -1,26 +1,63 @@
 package com.fundation.search.controller;
 
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
-public class Logger {
-    private static final Logger LOGGER = Logger.getLogger(LoggerExample.class.getName());
-    public static void main(String[] args) throws SecurityException, IOException {
-        LOGGER.info("Logger Name: "+LOGGER.getName());
+/**
+ * Class LoggerCreator to register events of the programm.
+ */
+public class LoggerCreator {
 
-        LOGGER.warning("Can cause ArrayIndexOutOfBoundsException");
-        //An array of size 3
-        int []a = {1,2,3};
-        int index = 4;
-        LOGGER.config("index is set to "+index);
-
-        try {
-            System.out.println(a[index]);
-        } catch(ArrayIndexOutOfBoundsException ex) {
-            LOGGER.log(Level.SEVERE, "Exception occur", ex);
-        }
-
+    private static final Logger LOGGER = Logger.getLogger(LoggerCreator.class.getName());
+    private String action;
+    private static LoggerCreator logInstance;
+    /**
+     * Constructor LoggerCreator.
+     * @param action the action executed.
+     */
+    private LoggerCreator(String action) {
+        this.action = action;
     }
+    /**
+     * Method getInstance to determina if exists an instance.
+     * @param action the action executed.
+     */
+    public static LoggerCreator getInstance(String action) {
+        if (logInstance == null) {
+            logInstance = new LoggerCreator(action);
+        }
+        return logInstance;
+    }
+    /**
+     * Method setLog to insert information in searchFiles log.
+     */
+    public void setLog() {
+        Handler consoleHandler = null;
+        Handler fileHandler  = null;
+        SimpleFormatter formatter = new SimpleFormatter();
+        try{
+            consoleHandler = new ConsoleHandler();
+            fileHandler  = new FileHandler("./searchFiles.log");
+
+            LOGGER.addHandler(consoleHandler);
+            LOGGER.addHandler(fileHandler);
+            fileHandler.setFormatter(formatter);
+            consoleHandler.setLevel(Level.ALL);
+            fileHandler.setLevel(Level.ALL);
+            LOGGER.setLevel(Level.ALL);
+            LOGGER.info(getAction());
+        } catch(IOException exception) {
+            LOGGER.log(Level.SEVERE, "Error ", exception);
+        }
+    }
+    /**
+     * Method getAction to get the action of the constructor.
+     * @return a string with the action description.
+     */
+    public String getAction() {
+        return action;
+    }
+
+
 }
 
