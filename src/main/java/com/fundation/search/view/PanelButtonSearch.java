dc7a57a05a6753d086fc0d18c32c903acf715d22
PanDeBatalla94 <mariarenee.canqui@gmail.com>
2018-04-12 18:41:28 -0400

    added documentation

--- src/main/java/com/fundation/search/view/PanelButtonSearch.java
+++ src/main/java/com/fundation/search/view/PanelButtonSearch.java
@@ -1,3 +1,9 @@
+/**
+ * @(#)PanelNormalSearch.java 03/27/18.
+ * Copyright (c) 2018 Jala Foundation.
+ * Cochabamba, Bolivia.
+ * Project Search for Prog102.
+ */
 package com.fundation.search.view;
 
 import info.clearthought.layout.TableLayout;
@@ -5,17 +11,25 @@ import info.clearthought.layout.TableLayoutConstraints;
 
 import javax.swing.JPanel;
 import javax.swing.JButton;
-
+/**
+ * Class to initialize panel for buttons.
+ *
+ * @version 27 Mar 2018  * @Daniel Caballero.
+ */
 public class PanelButtonSearch extends JPanel {
 
     JButton buttonSearch = new JButton();
     JButton buttonClear = new JButton();
 
-
+    /**
+     * Class constructor.
+     */
     public PanelButtonSearch() {
         initComponents();
     }
-
+    /**
+     * Initializes panel components.
+     */
     public void initComponents() {
         buttonSearch = new JButton();
         buttonClear = new JButton();
