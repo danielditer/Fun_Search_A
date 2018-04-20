d76e6c942a816bbe6f2f92dedf4281a086c260a5
JimmyRomero <jimmy.romero@fundacion-jala.org>
2018-04-20 13:22:17 -0400

    command line improved

--- src/main/java/com/fundation/search/controller/CommandController.java
+++ src/main/java/com/fundation/search/controller/CommandController.java
@@ -48,8 +48,6 @@ public class CommandController implements Controller {
 
         } else {
             for (int i = 0; i < inputCommands.length - 1; i++) {
-
-
                 System.out.println("x:" + inputCommands[i + 1]);
                 if (inputCommands[i].equals("-n")) { /* Search by name*/
                     searchCriteria.setName(inputCommands[i + 1]);
@@ -78,6 +76,31 @@ public class CommandController implements Controller {
                 }
                 if (inputCommands[i].equals("-type")) { /* Search by owner*/
                     searchCriteria.setTypeFile(Integer.parseInt(inputCommands[i + 1]));
+                }
+
+                if (inputCommands[i].equals("-dc")) { /* Search by date*/
+                    String[] date = inputCommands[i + 1].split("_");
+                    searchCriteria.setFromDate(date[0]);
+                    searchCriteria.setToDate(date[1]);
+                    searchCriteria.setCreateDate(true);
+                }
+                if (inputCommands[i].equals("-dm")) { /* Search by date*/
+                    String[] date = inputCommands[i + 1].split("_");
+                    searchCriteria.setFromDate(date[0]);
+                    searchCriteria.setToDate(date[1]);
+                    searchCriteria.setModifiedDate(true);
+                }
+                if (inputCommands[i].equals("-da")) { /* Search by date*/
+                    String[] date = inputCommands[i + 1].split("_");
+                    searchCriteria.setFromDate(date[0]);
+                    searchCriteria.setToDate(date[1]);
+                    searchCriteria.setAccessedDate(true);
+                }
+                if(inputCommands[i].equals("-size")){
+                    String[] size = inputCommands[i + 1].split(" ");
+                    searchCriteria.setSizeSign(size[0]);
+                    searchCriteria.setSizeRequired(size[1]);
+                    searchCriteria.setSizeMeasure(size[2]);
                 }
 
 
--- src/main/java/com/fundation/search/view/CommandResultView.java
+++ src/main/java/com/fundation/search/view/CommandResultView.java
@@ -7,6 +7,7 @@
 package com.fundation.search.view;
 
 import com.fundation.search.model.Asset;
+import com.fundation.search.model.ResultFile;
 
 import java.util.List;
 
@@ -39,12 +40,27 @@ public class CommandResultView {
             String heading1 = "FilePath";
             String heading2 = "FileName";
             String heading3 = "Hidden";
-            System.out.printf("%-15s %50s %30s %n", heading1, heading2, heading3);
+            String heading4 = "ReadOnly";
+            String heading5 = "Owner";
+            String heading6 = "CreationDate";
+            String heading7 = "ModifiedDate";
+            String heading8 = "AccessedDate";
+            String heading9 = "Size";
+
+            System.out.printf("%-15s %50s %15s %15s %15s %15s %15s %15s %15s %n", heading1, heading2, heading3, heading4, heading5, heading6, heading7, heading8, heading9);
             String divider = "---------------------------------------------------------------------------------------------------";
             System.out.println(divider);
             for (Asset results : resultFileList) {
                 System.out.println("");
-                System.out.printf("%-15s %30s %30s ", results.getPath(), results.getFileName(), results.getHidden());
+                if (results instanceof ResultFile) {
+                    System.out.printf("%-15s %30s %15s %15s %15s %15s %15s %15s %15s",
+                            results.getPath(), results.getFileName(), results.getHidden(), results.getReadOnly(), results.getOwner(), results.getCreationTime(), results.getLastModifiedTime(), results.getLastAccessTime(), results.getSize());
+
+                } else {
+                    System.out.printf("%-15s %30s %15s %15s %15s ",
+                            results.getPath(), results.getFileName(), results.getHidden(), results.getReadOnly(), results.getOwner());
+
+                }
 
             }
         }
