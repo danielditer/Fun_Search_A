9833f39dab46dc71cddfe2d4abc4d0cebc9a9395
Daniel Caballero <daniel.caballero@fundacion-jala.org>
2018-04-23 08:14:14 -0400

    updated frame rate panel

--- src/main/java/com/fundation/search/view/PanelMultimediaSearch.java
+++ src/main/java/com/fundation/search/view/PanelMultimediaSearch.java
@@ -8,19 +8,16 @@ import javax.swing.border.TitledBorder;
 
 public class PanelMultimediaSearch extends JPanel{
     private JPanel panelNamePath;
-    private JScrollPane scrollPane1;
-    private JPanel panelResults;
-    private JScrollPane scrollPane2;
-    private JTable table1;
     private JPanel panelDuration;
     private JLabel labelMoreThan;
     private JSpinner spinner1;
     private JLabel labelLessThan;
     private JSpinner spinner2;
     private JPanel panelFrameRate;
+    private JComboBox comboBoxFrameRate;
     private JLabel label5;
     private JSpinner spinner3;
-    private JLabel label6;
+    private JLabel labelFps;
     private JSpinner spinner4;
     private JPanel panelCodec;
     private JLabel label7;
@@ -41,10 +38,6 @@ public class PanelMultimediaSearch extends JPanel{
 
     private void initComponents() {
         panelNamePath = new PanelNamePath();
-        scrollPane1 = new JScrollPane();
-        panelResults = new JPanel();
-        scrollPane2 = new JScrollPane();
-        table1 = new JTable();
         panelDuration = new JPanel();
         labelMoreThan = new JLabel();
         spinner1 = new JSpinner();
@@ -53,7 +46,7 @@ public class PanelMultimediaSearch extends JPanel{
         panelFrameRate = new JPanel();
         label5 = new JLabel();
         spinner3 = new JSpinner();
-        label6 = new JLabel();
+        labelFps = new JLabel();
         spinner4 = new JSpinner();
         panelCodec = new JPanel();
         label7 = new JLabel();
@@ -66,12 +59,14 @@ public class PanelMultimediaSearch extends JPanel{
         panel8 = new JPanel();
         checkBox5 = new JCheckBox();
         checkBox6 = new JCheckBox();
+        comboBoxFrameRate = new JComboBox();
         panelButtonSearch = new PanelButtonSearch();
 
         //======== this ========
         setLayout(new TableLayout(new double[][] {
                 {TableLayout.PREFERRED, TableLayout.PREFERRED},
-                {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}}));
+                {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED,
+                        TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED}}));
 
         //======== panelNamePath ========
         add(panelNamePath, new TableLayoutConstraints(0, 0, 0, 0,
@@ -86,15 +81,20 @@ public class PanelMultimediaSearch extends JPanel{
 
             //---- labelMoreThan ----
             labelMoreThan.setText("More than");
-            panelDuration.add(labelMoreThan, new TableLayoutConstraints(0, 0, 0, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelDuration.add(spinner1, new TableLayoutConstraints(1, 0, 1, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelDuration.add(labelMoreThan, new TableLayoutConstraints(0, 0, 0, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelDuration.add(spinner1, new TableLayoutConstraints(1, 0, 1, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
             //---- labelLessThan ----
             labelLessThan.setText("Less than");
-            panelDuration.add(labelLessThan, new TableLayoutConstraints(2, 0, 2, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelDuration.add(spinner2, new TableLayoutConstraints(3, 0, 3, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelDuration.add(labelLessThan, new TableLayoutConstraints(2, 0, 2, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelDuration.add(spinner2, new TableLayoutConstraints(3, 0, 3, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
         }
-        add(panelDuration, new TableLayoutConstraints(0, 1, 0, 1, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panelDuration, new TableLayoutConstraints(0, 1, 0, 1,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
         //======== panelFrameRate ========
         {
@@ -105,38 +105,48 @@ public class PanelMultimediaSearch extends JPanel{
             ((TableLayout) panelFrameRate.getLayout()).setHGap(5);
             ((TableLayout) panelFrameRate.getLayout()).setVGap(5);
 
-            //---- label5 ----
-            label5.setText("More than");
-            panelFrameRate.add(label5, new TableLayoutConstraints(0, 0, 0, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelFrameRate.add(spinner3, new TableLayoutConstraints(1, 0, 1, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-
-            //---- label6 ----
-            label6.setText("Less than");
-            panelFrameRate.add(label6, new TableLayoutConstraints(2, 0, 2, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelFrameRate.add(spinner4, new TableLayoutConstraints(3, 0, 3, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            //---- labelFps ----
+            comboBoxFrameRate.addItem("All");
+            comboBoxFrameRate.addItem("23.97");
+            comboBoxFrameRate.addItem("25");
+            comboBoxFrameRate.addItem("29.97");
+            comboBoxFrameRate.addItem("30");
+            comboBoxFrameRate.addItem("60");
+            labelFps.setText("fps");
+            panelFrameRate.add(comboBoxFrameRate, new TableLayoutConstraints(1, 0, 1, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelFrameRate.add(labelFps, new TableLayoutConstraints(2, 0, 2, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
         }
-        add(panelFrameRate, new TableLayoutConstraints(0, 2, 0, 2, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panelFrameRate, new TableLayoutConstraints(0, 2, 0, 2,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
         //======== panelCodec ========
         {
             panelCodec.setBorder(new TitledBorder("Codec"));
             panelCodec.setLayout(new TableLayout(new double[][] {
-                    {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED},
+                    {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED,
+                            TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED},
                     {TableLayout.PREFERRED, TableLayout.PREFERRED}}));
             ((TableLayout) panelCodec.getLayout()).setHGap(5);
             ((TableLayout) panelCodec.getLayout()).setVGap(5);
 
             //---- label7 ----
             label7.setText("Video codec");
-            panelCodec.add(label7, new TableLayoutConstraints(0, 0, 0, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelCodec.add(textField3, new TableLayoutConstraints(1, 0, 3, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelCodec.add(label7, new TableLayoutConstraints(0, 0, 0, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelCodec.add(textField3, new TableLayoutConstraints(1, 0, 3, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
             //---- label8 ----
             label8.setText("Audio codec");
-            panelCodec.add(label8, new TableLayoutConstraints(4, 0, 4, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
-            panelCodec.add(textField4, new TableLayoutConstraints(5, 0, 7, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelCodec.add(label8, new TableLayoutConstraints(4, 0, 4, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panelCodec.add(textField4, new TableLayoutConstraints(5, 0, 7, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
         }
-        add(panelCodec, new TableLayoutConstraints(0, 3, 0, 3, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panelCodec, new TableLayoutConstraints(0, 3, 0, 3, TableLayoutConstraints.FULL,
+                TableLayoutConstraints.FULL));
 
         //======== panel5 ========
         {
@@ -146,12 +156,15 @@ public class PanelMultimediaSearch extends JPanel{
                     {TableLayout.PREFERRED}}));
             ((TableLayout)panel5.getLayout()).setHGap(5);
             ((TableLayout)panel5.getLayout()).setVGap(5);
-            panel5.add(comboBox1, new TableLayoutConstraints(0, 0, 0, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panel5.add(comboBox1, new TableLayoutConstraints(0, 0, 0, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
         }
-        add(panel5, new TableLayoutConstraints(0, 4, 0, 4, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panel5, new TableLayoutConstraints(0, 4, 0, 4,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
         //======== panelDate ========
-        add(panelDate, new TableLayoutConstraints(0, 5, 0, 5, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panelDate, new TableLayoutConstraints(0, 5, 0, 5,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
         //======== panel8 ========
         {
@@ -164,16 +177,20 @@ public class PanelMultimediaSearch extends JPanel{
 
             //---- checkBox5 ----
             checkBox5.setText("Read-only");
-            panel8.add(checkBox5, new TableLayoutConstraints(0, 0, 0, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panel8.add(checkBox5, new TableLayoutConstraints(0, 0, 0, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
             //---- checkBox6 ----
             checkBox6.setText("Hidden");
-            panel8.add(checkBox6, new TableLayoutConstraints(2, 0, 2, 0, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+            panel8.add(checkBox6, new TableLayoutConstraints(2, 0, 2, 0,
+                    TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
         }
-        add(panel8, new TableLayoutConstraints(0, 6, 0, 6, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panel8, new TableLayoutConstraints(0, 6, 0, 6,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
 
         //======== panelButtonSearch ========
 
-        add(panelButtonSearch, new TableLayoutConstraints(0, 7, 0, 7, TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
+        add(panelButtonSearch, new TableLayoutConstraints(0, 7, 0, 7,
+                TableLayoutConstraints.FULL, TableLayoutConstraints.FULL));
     }
 }
